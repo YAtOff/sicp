4ec24c634672c7e8036f87b1d8222201c3945fcd
Yavor Atov <yavor.atov@gmail.com>
2016-06-28 14:43:14 +0300

    Solved tasks for hw1

--- racket/hw1/hw1.rkt
+++ racket/hw1/hw1.rkt
@@ -10,18 +10,22 @@
     s
     (if (equal? w (first s))
       (word-removed w (bf s))
-      (se w (word-removed w (bf s)))))
+      (se (first s) (word-removed w (bf s)))))
 )
 (define (dupls-removed sent)
-  ; Your code here
-  (error "Not yet implemented")
+  (if (empty? sent)
+    sent
+    (se (first sent) (dupls-removed (word-removed (first sent) (bf sent)))))
 )
 
 ; Exercise 2 - Define count-word
 
 (define (count-word sent wd)
-  ; Your code here
-  (error "Not yet implemented")
+  (if (empty? sent)
+    0
+    (+
+      (if (equal? wd (first sent)) 1 0)
+      (count-word (bf sent) wd)))
 )
 
 ; Exercise 3
@@ -39,36 +43,59 @@
 
 ; Explain what would happen if you used new-if instead of if below.
 #|
-Your explanation here
+Infinite recursion
 
 |#
 
 ; Exercise 4 - Define squares
 
 (define (squares sent)
-  ; Your code here
-  (error "Not yet implemented")
+  (if (empty? sent)
+    sent
+    (se (* (first sent) (first sent))
+        (squares (bf sent))))
 )
 
 ; Exercise 5 - Define switch
 
 (define (switch sent)
-  ; Your code here
-  (error "Not yet implemented")
+  (define (switch-word w)
+    (cond 
+      ((equal? w 'you) 'me)
+      ((equal? w 'I) 'you)
+      ((equal? w 'me) 'you)
+      (else w))
+  )
+
+  (define (switch-sent sent)
+    (cond 
+      ((empty? sent) sent)
+      (else (se (switch-word (first sent)) (switch-sent (bf sent)))))
+  )
+
+  (cond 
+    ((empty? sent) sent)
+    ((equal? (first sent) 'you) (se 'I (switch-sent (bf sent))))
+    (else (switch-sent sent)))
 )
 
 ; Exercise 6 - Define ordered?
 
 (define (ordered? sent)
-  ; Your code here
-  (error "Not yet implemented")
+  (cond 
+    ((< (length sent) 2) #t)
+    (else (and 
+            (< (first sent) (first (bf sent)))
+            (ordered? (bf (bf sent))))))
 )
 
 ; Exercise 7 - Define ends-e
 
 (define (ends-e sent)
-  ; Your code here
-  (error "Not yet implemented")
+  (cond 
+    ((empty? sent) sent)
+    ((equal? (last (first sent)) 'e) (se (first sent) (ends-e (bf sent))))
+    (else (ends-e (bf sent))))
 )
 
 ; Exercise 8
@@ -76,5 +103,11 @@ Your explanation here
 #|
 
 Your explanation here
+
+(define (inf-rec)
+  (if #t (inf-rec) #f))
+
+(or #t (inf-rec))
+(and #f (inf-rec))
 
 |#
